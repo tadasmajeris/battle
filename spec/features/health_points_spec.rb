07442df3bfea716cfb87e58c9125f38fe8e76469
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-11 15:13:31 +0100

    Chapter 21 completed. Added web helpers to DRY up the tests.

--- spec/features/health_points_spec.rb
+++ spec/features/health_points_spec.rb
@@ -2,12 +2,7 @@ require 'spec_helper'
 
 RSpec.feature "Health Points", :type => :feature do
   scenario "Player 2 has hit points" do
-    visit "/"
-
-    fill_in "player1_name", :with => "Dave"
-    fill_in "player2_name", :with => "Timmy"
-    click_button "Submit Players"
-
+    sign_in_and_play
     expect(page).to have_content("Timmy HP: 100")
   end
 end
--- spec/features/naming_players_spec.rb
+++ spec/features/naming_players_spec.rb
@@ -2,12 +2,7 @@ require 'spec_helper'
 
 RSpec.feature "Naming Players", :type => :feature do
   scenario "Expects players to fill in their names" do
-    visit "/"
-
-    fill_in "player1_name", :with => "Dave"
-    fill_in "player2_name", :with => "Timmy"
-    click_button "Submit Players"
-
+    sign_in_and_play
     expect(page).to have_content("Dave vs. Timmy")
   end
 end
