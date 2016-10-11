878006206a69169e0a155af61c50c1d9710a5b12
Tom Nunn <t@tomnunn.uk>
2016-10-11 15:05:00 +0100

    Chapter 20: add view hit points example to player 2

--- spec/features/health_points_spec.rb
+++ spec/features/health_points_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-RSpec.feature "Naming Players", :type => :feature do
-  scenario "Expects players to fill in their names" do
+RSpec.feature "Health Points", :type => :feature do
+  scenario "Player 2 has hit points" do
     visit "/"
 
     fill_in "player1_name", :with => "Dave"
     fill_in "player2_name", :with => "Timmy"
     click_button "Submit Players"
 
-    expect(page).to have_content("Dave vs. Timmy")
+    expect(page).to have_content("Timmy HP: 100")
   end
 end
