c425a2957b954322bcb2262e9f3044e33026b2d4
James <James.Steel@me.com>
2016-10-11 11:44:04 +0100

    player hit points

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,19 @@
+feature 'View hit points' do
+
+ scenario 'see Player 2 hit points' do
+  visit('/')
+  fill_in :player_1_name, with: 'Dave'
+  fill_in :player_2_name, with: 'Mittens'
+  click_button 'Submit'
+  expect(page).to have_content 'Mittens: 60hp'
+ end
+
+ scenario 'see Player 1 hit points' do
+   visit('/')
+   fill_in :player_1_name, with: 'Dave'
+   fill_in :player_2_name, with: 'Mittens'
+   click_button 'Submit'
+   expect(page).to have_content 'Dave: 60hp'
+ end
+
+end
